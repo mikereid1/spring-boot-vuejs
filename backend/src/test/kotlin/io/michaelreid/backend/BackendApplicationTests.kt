b89fd3d5f15fba4c1b8208224f8b03ce82a837f1
mike <michaelreid87@protonmail.com>
2022-06-09 18:49:19 +0200

    initial commit

---
+++ backend/src/test/kotlin/io/michaelreid/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.michaelreid.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
