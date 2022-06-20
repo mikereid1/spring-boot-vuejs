23ded2dd3a082d35c55f56ef39829ad89fad916b
mike <michaelreid87@protonmail.com>
2022-06-20 18:58:54 +0200

    add href to current job

--- frontend/src/components/TheBody.vue
+++ frontend/src/components/TheBody.vue
@@ -10,17 +10,19 @@ export default {
 </script>
 
 <template>
-  <p>
-    I am a software developer with several years of experience working with Java and Kotlin.<br>
-    The majority of my time has been spent working on backends using Spring Boot.<br>
-    I enjoy learning new things and I am always improving what I know to expand my toolset.<br>
-    Currently I work as a backend developer for Spread Group.
-  </p>
+  <div>
+    <p>
+      I am a software developer with several years of experience working with Java and Kotlin.<br>
+      The majority of my time has been spent working on backends using Spring Boot.<br>
+      I enjoy learning new things and I am always improving what I know to expand my toolset.<br>
+      Currently I work as a backend developer for <a href="">Spread Group</a>.
+    </p>
+  </div>
   <skill-list/>
 </template>
 
 <style scoped>
-p {
+div {
   display: flex;
   flex-direction: column;
   justify-content: center;
